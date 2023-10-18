77bc45d7468c5c9fa61ef53bb142b803bab511ae
Rodrigo Martinez <ropecos94@gmail.com>
2023-10-18 13:53:06 -0300

    Update validacion.js

---
+++ js/validacion.js
@@ -0,0 +1,11 @@
+
+const form = document.querySelector('form');
+const lastName = document.getElementById('nombre');
+const name = document.getElementById('apellido');
+const email = document.getElementById('email');
+const password1 = document.getElementById('password1');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit',function(e){
+    
+})
