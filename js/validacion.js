b82ee90b46b818611689cae028c4a447aaca1cf2
[MelaniePorley] <melanie.porley@gmail.com>
2023-10-18 21:36:13 -0300

    some fixes

--- js/validacion.js
+++ js/validacion.js
@@ -4,7 +4,7 @@
   
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation');
-  
+    
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
@@ -18,36 +18,74 @@
         const firstName = document.getElementById('nombre');
         const lastName = document.getElementById('apellido');
         const terms = document.getElementById('terminos');
+        const btnTerms = document.getElementById('btnTerms');
+        const email = document.getElementById('email');
         
-        if(password1.value.length < 6 || password1.value !== password2.value){
+        
+        if(password1.value.length < 6){
             event.preventDefault();
+            password1.classList.remove('is-valid');
             password1.classList.add('is-invalid');
+           
         } else {
             password1.classList.remove('is-invalid');
+            password1.classList.add('is-valid');
+
+        }
+
+        if(password1.value !== password2.value || !password1.checkValidity()){
+            event.preventDefault();
+            password2.classList.remove('is-valid');
+            password2.classList.add('is-invalid');
+        } else {
+            password2.classList.remove('is-invalid');
+            password2.classList.add('is-valid');
         }
         
         if(firstName.value === ''){
             event.preventDefault();
+            firstName.classList.remove('is-valid');
             firstName.classList.add('is-invalid');
         } else {
             firstName.classList.remove('is-invalid');
+            firstName.classList.add('is-valid');
         }
         
         if(lastName.value === ''){
             event.preventDefault();
+            lastName.classList.remove('is-valid');
             lastName.classList.add('is-invalid');
         } else {
             lastName.classList.remove('is-invalid');
+            lastName.classList.add('is-valid');
         }
         
         if(!terms.checked){
             event.preventDefault();
+            terms.classList.remove('is-valid');
             terms.classList.add('is-invalid');
+            btnTerms.classList.add('is-invalid');
         } else {
-            terms.classList.remove('is-invalid');
+            terms.classList.add('is-valid');
+            btnTerms.classList.remove('is-invalid');
+        }
+        if(email.value === '' && !checkEmail(email)){
+            event.preventDefault();
+            email.classList.remove('is-valid');
+            email.classList.add('is-invalid');
+        } else {
+            email.classList.remove('is-invalid');
+            email.classList.add('is-valid');
+        }
+
+        if (password1.value.length >= 6 && password1.value === password2.value && firstName.value !== '' && lastName.value !== '' && terms.checked && email.value !== '' && checkEmail(email)) {
+            form.classList.add('was-validated');
+            alert("Datos enviados");
+        } else {
+            alert("Error en los datos");
         }
   
-        form.classList.add('was-validated');
+       
       }, false);
     });
     const inputs = document.querySelectorAll('input');
@@ -58,3 +96,10 @@
     });
   })();
   
+  function checkEmail(email){
+if(email.checkValidity()){
+    return true
+}else {
+    return false
+}
+  };
